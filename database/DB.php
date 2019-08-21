443238ac5e1e3c0038416bcaa42ed58033228d0b
ram bhosale <rambhosale9@gmail.com>
2019-08-21 13:54:46 +0530

    added required config files

--- database/DB.php
+++ database/DB.php
@@ -55,32 +55,12 @@ function &DB($params = '', $query_builder_override = NULL)
 	{
 		// Is the config file in the environment folder?
 		if ( ! file_exists($file_path = APPPATH.'config/'.ENVIRONMENT.'/database.php')
-			&& ! file_exists($file_path = APPPATH.'config/database.php'))
+			&& ! file_exists($file_path = DBPATH))
 		{
 			show_error('The configuration file database.php does not exist.');
 		}
 
 		include($file_path);
-
-		// Make packages contain database config files,
-		// given that the controller instance already exists
-		if (class_exists('CI_Controller', FALSE))
-		{
-			foreach (get_instance()->load->get_package_paths() as $path)
-			{
-				if ($path !== APPPATH)
-				{
-					if (file_exists($file_path = $path.'config/'.ENVIRONMENT.'/database.php'))
-					{
-						include($file_path);
-					}
-					elseif (file_exists($file_path = $path.'config/database.php'))
-					{
-						include($file_path);
-					}
-				}
-			}
-		}
 
 		if ( ! isset($db) OR count($db) === 0)
 		{
@@ -199,7 +179,7 @@ function &DB($params = '', $query_builder_override = NULL)
 	// Instantiate the DB adapter
 	$driver = 'CI_DB_'.$params['dbdriver'].'_driver';
 	$DB = new $driver($params);
-
+	
 	// Check for a subdriver
 	if ( ! empty($DB->subdriver))
 	{
